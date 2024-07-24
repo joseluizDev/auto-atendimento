595751ef2fb31e932a0b9a3d190d651722f287a4
jose luiz <lui_zzzz@hotmail.com>
2024-07-24 14:08:24 -0300

    chore: Add type hinting stub files for pip dependencies

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print('hello world')
+import time
+from datetime import datetime
+
+import schedule
+
+
+def enviar_mensagem_de_amor():
+    mensagens = [
+        "Bom dia",
+
+    ]
+    mensagem = mensagens[datetime.now().day % len(mensagens)]
+    print(mensagem)
+
+
+schedule.every().day.at("14:08").do(enviar_mensagem_de_amor)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
