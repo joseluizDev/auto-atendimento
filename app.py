3f690db4073f71b84dd41d6689d14f59617c8cc5
jose luiz <lui_zzzz@hotmail.com>
2024-07-25 09:35:37 -0300

    feat: Update app.py to fix phone number format for sending WhatsApp messages

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from mensagens import mensagens_de_amor
 BASE_URL = 'https://server-evolutionapi.uwqcav.easypanel.host'
 API_KEY = 'Jr59IRl66nL0sJtq2EmBfQNMVQqwvKyi'
 CHAT = 'JoseLuiz'
-telefone = '559492883002'
+telefone = '5522981484436'
 telefone2 = '553299644257'
-
+telefone3 = '5511963866977'
 headers = {
     'Apikey': API_KEY,
 }
@@ -38,6 +38,16 @@ while True:
             "presence": "composing"
         }
     }
+    body3 = {
+        "number": f"{telefone3}@s.whatsapp.net",
+        "textMessage": {
+            "text": f"{mensagem.replace('\n', '\\n')}"
+        },
+        "options": {
+            "delay": 1200,
+            "presence": "composing"
+        }
+    }
 
     response = requests.post(
         f'{BASE_URL}/message/sendText/{CHAT}',
@@ -48,6 +58,12 @@ while True:
     response = requests.post(
         f'{BASE_URL}/message/sendText/{CHAT}',
         json=body2,
+        headers=headers,
+    )
+
+    response = requests.post(
+        f'{BASE_URL}/message/sendText/{CHAT}',
+        json=body3,
         headers=headers,
     )
 
