43f24c828da48aaf45e489237c9017e54b4770d2
jose luiz <lui_zzzz@hotmail.com>
2024-07-25 12:26:41 -0300

    feat: Refactor app.py to improve WhatsApp message sending functionality

--- app.py
+++ app.py
@@ -1,72 +1,18 @@
-import random
-import time
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
-import requests
+app = FastAPI()
 
-from mensagens import mensagens_de_amor
 
-BASE_URL = 'https://server-evolutionapi.uwqcav.easypanel.host'
-API_KEY = 'Jr59IRl66nL0sJtq2EmBfQNMVQqwvKyi'
-CHAT = 'JoseLuiz'
-telefone = '5522981484436'
-telefone2 = '553299644257'
-telefone3 = '5511963866977'
-headers = {
-    'Apikey': API_KEY,
-}
+class WebhookData(BaseModel):
+    key: str
 
-while True:
-    mensagem = random.choice(mensagens_de_amor)
-    print(mensagem)
-    body = {
-        "number": f"{telefone}@s.whatsapp.net",
-        "textMessage": {
-            "text": f"{mensagem.replace('\n', '\\n')}"
-        },
-        "options": {
-            "delay": 1200,
-            "presence": "composing"
-        }
-    }
-    body2 = {
-        "number": f"{telefone2}@s.whatsapp.net",
-        "textMessage": {
-            "text": f"{mensagem.replace('\n', '\\n')}"
-        },
-        "options": {
-            "delay": 1200,
-            "presence": "composing"
-        }
-    }
-    body3 = {
-        "number": f"{telefone3}@s.whatsapp.net",
-        "textMessage": {
-            "text": f"{mensagem.replace('\n', '\\n')}"
-        },
-        "options": {
-            "delay": 1200,
-            "presence": "composing"
-        }
-    }
 
-    response = requests.post(
-        f'{BASE_URL}/message/sendText/{CHAT}',
-        json=body,
-        headers=headers,
-    )
+@app.post("/webhook")
+async def webhook(data: WebhookData):
+    print("Dados recebidos:", data)
+    return {"status": "sucesso", "message": "Webhook recebido!"}
 
-    response = requests.post(
-        f'{BASE_URL}/message/sendText/{CHAT}',
-        json=body2,
-        headers=headers,
-    )
-
-    response = requests.post(
-        f'{BASE_URL}/message/sendText/{CHAT}',
-        json=body3,
-        headers=headers,
-    )
-
-    print(response.json())
-
-    time.sleep(600)
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8000)
